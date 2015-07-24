789424a8457d47209f50c693a2c1beb06e1839d7
Orange Owl Solutions <orangeowlsolutions@gmail.com>
2015-07-24 14:44:04 +0200

    Create DisplayImage.m

---
+++ BP_FBP_FFBP/DisplayImage.m
@@ -0,0 +1,14 @@
+function DisplayImage(figure_nr, x_vec, y_vec, Reconstruction, dyn_range, ImageTitle, FontSize)
+
+figure(figure_nr)
+imagesc(x_vec, y_vec, 20*log10(abs(Reconstruction)/max(max(abs(Reconstruction)))),[-dyn_range 0])
+axis xy image;
+h=title(ImageTitle);
+set(h, 'FontSize', FontSize);
+h = xlabel('x (m)');
+set(h, 'FontSize', FontSize);
+h = ylabel('y (m)');
+set(h, 'FontSize', FontSize);
+colorbar('hide');
+colormap ('hot');
+set(gca, 'FontSize', FontSize);
