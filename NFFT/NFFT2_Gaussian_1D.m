8ba66d9df2ce9c7e7c5dc10e6c2ae8dde2b17a0a
Orange Owl Solutions <orangeowlsolutions@gmail.com>
2015-09-01 10:47:34 +0200

    Create NFFT2_Gaussian_1D.m

---
+++ NFFT/NFFT2_Gaussian_1D.m
@@ -0,0 +1,61 @@
+function result=NFFT2_Gaussian_1D(data, x, M)
+
+N = length(data);
+
+%--- Algorithm parameters
+b = log(10^(0.135));
+c = 2;                                                  % Oversampling factor >=1 (typically 2)
+K = fix(2 * b * pi);                                    % 2K+1 interpolation samples (N should be >> K) (parameter q/2 in Dutt & Rokhlin)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CONSTRUCTING THE SPECTRAL WINDOW %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[KK, X] = meshgrid(-K : K, x);
+M_U     = round(c * X);                                          
+phi_cap = exp( -(c * X - (M_U + KK)) .^ 2 / (4 * b)) / (2 * sqrt(b * pi));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CONSTRUCTING THE SPATIAL WINDOW %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+kk = -M / 2 : M / 2 - 1;
+xi = 2 * pi * kk / (c * M);
+phi = exp(-b * xi .^ 2);
+
+%%%%%%%%%%
+% STEP 1 %
+%%%%%%%%%%
+
+%--- Old version
+% u=zeros(1,c*M);
+% PP=mod(M_U+KK+M*c/2,c*M)+1;
+% for k=1:N 
+%     u(PP(k,(-K:K)+K+1))=u(PP(k,(-K:K)+K+1))+data(k)*phi_cap(k,(-K:K)+K+1);
+% end
+
+% --- New version
+u=zeros(1,c*M);
+for s=-M:M-1
+    for l=1:N
+        phi_cap=0;
+        if (abs(round(c*x(l))-s)<=K)
+            phi_cap = exp( -(c * x(l) - s) .^ 2 / (4 * b)) / (2 * sqrt(b * pi));
+        elseif (abs(round(2*x(l))+c*M-s)<=K)
+            phi_cap = exp( -(c * x(l) - s + c * M) .^ 2 / (4 * b)) / (2 * sqrt(b * pi));
+        elseif (abs(round(2*x(l))-c*M-s)<=K)
+            phi_cap = exp( -(c * x(l) - s - c * M) .^ 2 / (4 * b)) / (2 * sqrt(b * pi));
+        end
+        u(s+M+1)=u(s+M+1)+data(l)*phi_cap;
+    end
+end
+
+%%%%%%%%%%
+% STEP 2 %
+%%%%%%%%%%
+
+FFT_Mc = fftshift(fft(ifftshift(u)));
+
+%%%%%%%%%%
+% STEP 3 %
+%%%%%%%%%%
+
+result = FFT_Mc(1 + (c - 1) * M / 2 : (c + 1) * M / 2) ./ phi;
