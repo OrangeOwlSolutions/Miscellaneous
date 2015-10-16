b1d9f400b67d8e7fd59ecfe402ad65b346e6acdc
Orange Owl Solutions <orangeowlsolutions@gmail.com>
2015-10-16 10:51:59 +0200

    Delete NDFT2_2D.cuh

--- NFFT/NDFT2_2D.cuh
+++
@@ -1,8 +0,0 @@
-#ifndef NDFT2_2D_CUH
-#define NDFT2_2D_CUH
-
-extern "C" void NDFT2_2D_GPU(cublasHandle_t, const double * __restrict__, const double * __restrict__, const double * __restrict__, 
-							 const double * __restrict__, const double2_ * __restrict__, double2_ * __restrict__, const int, const int, 
-							 const int, const int);
-
-#endif
