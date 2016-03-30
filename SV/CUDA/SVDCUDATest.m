13c6a3068b6c2e87999298b7f7674714a25d8ca1
Orange Owl Solutions <orangeowlsolutions@gmail.com>
2016-03-30 10:06:28 +0200

    Update SVDCUDATest.m

--- SV/CUDA/SVDCUDATest.m
+++ SV/CUDA/SVDCUDATest.m
@@ -6,7 +6,7 @@ Ncols           = 8;
 numMatrices     = 1024;
 
 A           = load('inputMatrices.txt');
-S           = full_svd(A, 10, 8, numMatrices);
+S           = batchedMatlabSVD(A, 10, 8, numMatrices);
 Sapproach   = load('singularValues.txt');
 
 RelativeErrorPercentage = 100 * (sum((S - Sapproach).^2) / sum(S.^2))
