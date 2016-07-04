02cbcb5340ffeeb2e7254e27a64e3a69b28a34f7
Orange Owl <orangeowlsolutions@gmail.com>
2016-07-04 14:28:21 +0200

    Update NFFT3_1D.m

--- NFFT/NFFT3_1D.m
+++ NFFT/NFFT3_1D.m
@@ -1,12 +1,10 @@
-function result = NFFT3_1D(data, x, s)
+function result = NFFT3_1D(data, x, s, c, K)
 
 N = length(data);
 M = N;
 S = max(abs(s));
 
 %--- Algorithm parameters
-c=2;                                                    % Oversampling factor >=1 (typically 2)
-K=3;                                                    % 2K+1 interpolation samples (N should be >> K)
 alfa=(2-1/c)*pi-0.01;                                   % Half-size of the support of the interpolation window
 
 x1 = S * x / pi;
@@ -15,8 +13,7 @@ x1 = S * x / pi;
 % CONSTRUCTING THE SPATIAL WINDOW %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-phi2 = besseli(0, K * sqrt(alfa .^2 - (pi * s / (c * S)) .^2 )) / besseli(0, K * alfa);
-% phi2 = besseli(0, K * sqrt(alfa .^2 - (pi * s / (c * S)) .^2 ));
+phi2 = besseli(0, K * sqrt(alfa .^2 - (pi * s / (c * S)) .^2 ));
 
 %%%%%%%%%%%%%%%
 % CONVOLUTION %
@@ -29,31 +26,53 @@ phi2 = besseli(0, K * sqrt(alfa .^2 - (pi * s / (c * S)) .^2 )) / besseli(0, K *
 %     u(PP(k, (-K : K) + K + 1)) = u(PP(k, (-K : K) + K + 1)) + data(k) * spectrum_phi1(k, (-K : K) + K + 1);
 % end
 
-u = zeros(1, c * M);
-for k=-M:M-1
-    for l=1:N
-        phi_cap=0;
+u = zeros(1, 2 * M);
+phi_cap = zeros(N, 2 * M);
+for k = -M : M - 1
+    for l = 1 : N
+        phi_cap(l, k+M+1) =0;
         if (abs(round(c*x1(l))-k)<=K)
             pp=sqrt(K.^2-(c*x1(l)-k).^2);
-            phi_cap=(1/pi)*sinh(alfa*pp)./pp;
-            if pp==0 phi_cap=alfa/pi; end
-        elseif (abs(round(c*x1(l))+c*M-k)<=K)
-            pp=sqrt(K.^2-(c*x1(l)-k+c*M).^2);
-            phi_cap=(1/pi)*sinh(alfa*pp)./pp;
-            if pp==0 phi_cap=alfa/pi; end
-        elseif (abs(round(c*x1(l))-c*M-k)<=K)
-            pp=sqrt(K.^2-(c*x1(l)-k-c*M).^2);
-            phi_cap=(1/pi)*sinh(alfa*pp)./pp;
-            if pp==0 phi_cap=alfa/pi; end
+            phi_cap(l, k+M+1)=(1/pi)*sinh(alfa*pp)./pp;
+            if pp==0 phi_cap(l, k+M+1)=alfa/pi; end
+%         elseif (abs(round(c*x1(l))+2*M-k)<=K)
+%             pp=sqrt(K.^2-(c*x1(l)-k+2*M).^2);
+%             phi_cap=(1/pi)*sinh(alfa*pp)./pp;
+%             if pp==0 phi_cap=alfa/pi; end
+%         elseif (abs(round(c*x1(l))-2*M-k)<=K)
+%             pp=sqrt(K.^2-(c*x1(l)-k-2*M).^2);
+%             phi_cap=(1/pi)*sinh(alfa*pp)./pp;
+%             if pp==0 phi_cap=alfa/pi; end
         end
-        u(k+M+1)=u(k+M+1)+data(l)*phi_cap / besseli(0, K * alfa);
-%         u(k+M+1)=u(k+M+1)+data(l)*phi_cap;
+%         u(k+M/2+1)=u(k+M/2+1)+data(l)*phi_cap / besseli(0, K * alfa);
+        u(k+M+1)=u(k+M+1)+data(l)*phi_cap(l, k+M+1);
     end
 end
+
+% u = zeros(1, c * M);
+% phi_cap = zeros(N, c * M);
+% for k = -c*M/2 : c*M/2 - 1
+%     for l = 1 : N
+%         phi_cap(l, k+c*M/2+1) =0;
+%         if (abs(round(c*x1(l))-k)<=K)
+%             pp=sqrt(K.^2-(c*x1(l)-k).^2);
+%             phi_cap(l, k+M+1)=(1/pi)*sinh(alfa*pp)./pp;
+%             if pp==0 phi_cap(l, k+c*M/2+1)=alfa/pi; end
+%         elseif (abs(round(c*x1(l))+c*M-k)<=K)
+%             pp=sqrt(K.^2-(c*x1(l)-k+c*M).^2);
+%             phi_cap=(1/pi)*sinh(alfa*pp)./pp;
+%             if pp==0 phi_cap=alfa/pi; end
+%         elseif (abs(round(c*x1(l))-c*M-k)<=K)
+%             pp=sqrt(K.^2-(c*x1(l)-k-c*M).^2);
+%             phi_cap=(1/pi)*sinh(alfa*pp)./pp;
+%             if pp==0 phi_cap=alfa/pi; end
+%         end
+%         u(k+c*M/2+1)=u(k+c*M/2+1)+data(l)*phi_cap(l, k+c*M/2+1);
+%     end
+% end
 
 %%%%%%%%%%%%%
 % NER NUFFT %
 %%%%%%%%%%%%%
 
-result = NFFT1_1D(u, s * N / (c * S)) ./ phi2;
-
+result = NFFT1_1D(u, s * N / (c * S), c, K) ./ phi2;
