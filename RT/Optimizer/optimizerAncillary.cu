d4ffef898f39c7e670a9f396b1bd2b9036b37ef2
Orange Owl <orangeowlsolutions@gmail.com>
2016-10-07 09:45:31 +0200

    Delete optimizerAncillary.cu

--- RT/Optimizer/optimizerAncillary.cu
+++
@@ -1,8 +0,0 @@
-#ifndef __PROJECTONCURVE_CPP__
-#define __PROJECTONCURVE_CPP__
-
-#include <vector>
-
-void project_on_curve(const std::vector<double> &,  std::vector<double> &, std::vector<double> &, double &, std::vector<double> &);
-
-#endif
