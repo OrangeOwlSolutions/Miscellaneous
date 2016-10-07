df7afc9a840a941d7be7b382d815de36d68450ad
Orange Owl <orangeowlsolutions@gmail.com>
2016-10-07 09:46:38 +0200

    Create projectOnCurve.h

---
+++ RT/Optimizer/projectOnCurve.h
@@ -0,0 +1,8 @@
+#ifndef __PROJECTONCURVE_CPP__
+#define __PROJECTONCURVE_CPP__
+
+#include <vector>
+
+void project_on_curve(const std::vector<double> &,  std::vector<double> &, std::vector<double> &, double &, std::vector<double> &);
+
+#endif
