c16affc46938d9dd8140dc0ed472bb71d8a17722
Orange Owl <orangeowlsolutions@gmail.com>
2019-01-30 14:11:30 +0100

    Update NFFT3_1D_Opt.m

--- NFFT/NFFT3/NFFT3_1D_Opt.m
+++ NFFT/NFFT3/NFFT3_1D_Opt.m
@@ -1,4 +1,4 @@
-function result = NFFT3_1D_Opt(data, x, s)
+function result = NFFT3_1D_Opt(data, x, s, c, K, Max_Num_PFs, SBP_factor)
 
 xsi_max = pi;
 N = length(data);
@@ -6,8 +6,6 @@ M = N;
 S = max(abs(s));
 
 %--- Algorithm parameters
-c=2;                                                    % Oversampling factor >=1 (typically 2)
-K=3;                                                    % 2K+1 interpolation samples (N should be >> K)
 alfa=(2-1/c)*pi-0.01;                                   % Half-size of the support of the interpolation window
 alfa_prime=((2-1/c)*pi);
 
@@ -23,17 +21,25 @@ x1 = S * x / pi;
 % --- Calculating Prolate Spheroidal Wave Functions (PSWFs), Legendre Polynomials
 % and expansion coefficients of Legendre Polynomials yielding the (PSWFs)
 % Space_Bandwidth_Product=0.991525423728813*((2*pi-pi/c)*K);
-Space_Bandwidth_Product=(1*(2*pi-pi/c)*K);
-[PSWFs P V_even V_odd K_Leg] = S0n(Space_Bandwidth_Product, 12, (pi * s / (c * S)) / (2 * pi - pi / c), 1e-30);
+Space_Bandwidth_Product=(SBP_factor*(2*pi-pi/c)*K);
+[PSWFs P V_even V_odd K_Leg] = S0n(Space_Bandwidth_Product, 2 * Max_Num_PFs, (pi * s / (c * S)) / (2 * pi - pi / c), 1e-30);
 PSWFs = PSWFs.';
 
-load Result_c_2_NumLegPol_12_K_3_SBPfactor_1.mat
+filename = strcat('Result_c_', num2str(c), '_NumPFs_', num2str(Max_Num_PFs), '_K_',num2str(K), '_SBPfactor_', num2str(SBP_factor), '.mat');
+load(filename)
 
 % --- Recovering the "optimal" spatial window by the Legendre Polynomials
 phi2 = zeros(1, length(s));
 for p=0:2:K_Leg-1
-    phi2 = phi2 + sum(V_even(p/2+1,1:length(c_opt)).*c_opt) * P(p+1,:);
+    phi2 = phi2 + sum(V_even(p / 2 + 1, 1 : length(c_opt)) .* c_opt) * P(p + 1, :);
 end
+
+% phi = zeros(1, length(xi));
+% for p=0:2:K_Leg-1
+%     phi = phi + sum(V_even(p/2+1,1:length(c_opt)).*c_opt) * P(p+1,:);
+% end
+
+% phi2 = besseli(0, K * sqrt(alfa .^2 - (pi * s / (c * S)) .^2 )) / besseli(0, K * alfa);
 
 %%%%%%%%%%%%%%%
 % CONVOLUTION %
@@ -46,7 +52,29 @@ end
 %     u(PP(k, (-K : K) + K + 1)) = u(PP(k, (-K : K) + K + 1)) + data(k) * spectrum_phi1(k, (-K : K) + K + 1);
 % end
 
-u=zeros(1,c*M);
+% u=zeros(1,c*M);
+% for k=-c*M/2:c*M/2-1
+%     for l=1:N
+%         phi_cap=0;
+%         if (abs(round(c*x1(l))-k)<=K)
+%             for p=0:2:K_Leg-1
+%                 phi_cap = phi_cap + alfa_prime * (sqrt(p+0.5)/(2*pi)) * sum(V_even(p/2+1,1:length(c_opt)).*c_opt) * ((2 * (-1i)^p .* sqrt(pi./(2.*alfa_prime*abs(c*x(l)*S/pi-k))) .* besselj(p+0.5,alfa_prime*abs(c*x(l)*S/pi-k)))).';
+%             end
+%         elseif (abs(round(c*x1(l))+c*M/2-k)<=K)
+%             for p=0:2:K_Leg-1
+%                 phi_cap = phi_cap + alfa_prime * (sqrt(p+0.5)/(2*pi)) * sum(V_even(p/2+1,1:length(c_opt)).*c_opt) * ((2 * (-1i)^p .* sqrt(pi./(2.*alfa_prime*abs(c*x(l)*S/pi-k+c*M))) .* besselj(p+0.5,alfa_prime*abs(c*x(l)*S/pi-k+c*M)))).';
+%             end
+%         elseif (abs(round(c*x1(l))-c*c*M/2-k)<=K)
+%             for p=0:2:K_Leg-1
+%                 phi_cap = phi_cap + alfa_prime * (sqrt(p+0.5)/(2*pi)) * sum(V_even(p/2+1,1:length(c_opt)).*c_opt) * ((2 * (-1i)^p .* sqrt(pi./(2.*alfa_prime*abs(c*x(l)*S/pi-k-c*M))) .* besselj(p+0.5,alfa_prime*abs(c*x(l)*S/pi-k-c*M)))).';
+%             end
+%         end
+% %         u(k+M+1)=u(k+M+1)+data(l)*phi_cap(l,k+M+1);
+%         u(k+c*M/2+1)=u(k+c*M/2+1)+data(l)*phi_cap;
+%     end
+% end
+
+u=zeros(1,2*M);
 for k=-M:M-1
     for l=1:N
         phi_cap(l,k+M+1)=0;
@@ -54,14 +82,16 @@ for k=-M:M-1
             for p=0:2:K_Leg-1
                 phi_cap(l,k+M+1) = phi_cap(l,k+M+1) + alfa_prime * (sqrt(p+0.5)/(2*pi)) * sum(V_even(p/2+1,1:length(c_opt)).*c_opt) * ((2 * (-1i)^p .* sqrt(pi./(2.*alfa_prime*abs(c*x(l)*S/pi-k))) .* besselj(p+0.5,alfa_prime*abs(c*x(l)*S/pi-k)))).';
             end
-        elseif (abs(round(c*x1(l))+c*M-k)<=K)
+        elseif (abs(round(c*x1(l))+2*M-k)<=K)
             for p=0:2:K_Leg-1
                 phi_cap(l,k+M+1) = phi_cap(l,k+M+1) + alfa_prime * (sqrt(p+0.5)/(2*pi)) * sum(V_even(p/2+1,1:length(c_opt)).*c_opt) * ((2 * (-1i)^p .* sqrt(pi./(2.*alfa_prime*abs(c*x(l)*S/pi-k+c*M))) .* besselj(p+0.5,alfa_prime*abs(c*x(l)*S/pi-k+c*M)))).';
             end
-        elseif (abs(round(c*x1(l))-c*M-k)<=K)
+%             phi_cap=phi_cap.*exp(-1i*2*pi*s*M/(c*S));
+        elseif (abs(round(c*x1(l))-2*M-k)<=K)
             for p=0:2:K_Leg-1
                 phi_cap(l,k+M+1) = phi_cap(l,k+M+1) + alfa_prime * (sqrt(p+0.5)/(2*pi)) * sum(V_even(p/2+1,1:length(c_opt)).*c_opt) * ((2 * (-1i)^p .* sqrt(pi./(2.*alfa_prime*abs(c*x(l)*S/pi-k-c*M))) .* besselj(p+0.5,alfa_prime*abs(c*x(l)*S/pi-k-c*M)))).';
             end
+%             phi_cap=phi_cap.*exp(1i*2*pi*s*M/(c*S));
         end
         u(k+M+1)=u(k+M+1)+data(l)*phi_cap(l,k+M+1);
     end
@@ -71,4 +101,6 @@ end
 % NER NUFFT %
 %%%%%%%%%%%%%
 
-result = NFFT1_1D_Optimized(u, s * N / (c * S)) ./ phi2;
+result = NFFT1_1D_Opt(u, s * N / (c * S), c, K, Max_Num_PFs, SBP_factor) ./ phi2;
+
+
