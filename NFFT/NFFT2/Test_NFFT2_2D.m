c0d736d34d24b60cf8950d8d374c4350e856752c
Orange Owl <orangeowlsolutions@gmail.com>
2019-01-30 11:38:11 +0100

    Update and rename NFFT/Test_NFFT2_2D.m to NFFT/NFFT2/Test_NFFT2_2D.m

--- NFFT/NFFT2/Test_NFFT2_2D.m
+++ NFFT/NFFT2/Test_NFFT2_2D.m
@@ -17,10 +17,16 @@ y = M_y * (lambda / 2) * (rand(1, M_x * M_y) - 0.5);
 
 data = (rand(1, M_x * M_y) - 0.5) + 1i * (rand(1, M_x * M_y) - 0.5);
 
+Max_Num_PFs = 20;
+SBP_factor  = 1.1;
+c = 2;
+K = 6;
 transf_1 = NDFT2_2D(data, x, y, N, M);
-transf_2 = NUFFT_NED_2D_Matlab(data, y, x, M, N);
+transf_2 = NFFT2_2D(data, y, x, M, N);
+transf_3 = NFFT2_2D_Opt(data, y, x, M, N, c, K, Max_Num_PFs, SBP_factor);
 
-100*sqrt(sum(sum(abs(transf_1 - transf_2).^2)) / sum(sum(abs(transf_1).^2)))
+100 * sqrt(sum(sum(abs(transf_1 - transf_2).^2)) / sum(sum(abs(transf_1).^2)))
+100 * sqrt(sum(sum(abs(transf_1 - transf_3).^2)) / sum(sum(abs(transf_1).^2)))
 
 figure(1)
 imagesc(abs(transf_1)), colorbar
