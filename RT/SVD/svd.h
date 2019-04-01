96414bd01e9c8eb386fd2422d3cc8ae5f6ac6acf
Orange Owl <orangeowlsolutions@gmail.com>
2019-04-01 14:08:38 +0200

    Delete svd.h

--- RT/SVD/svd.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SVD_H__
-#define __SVD_H__
-
-#include <thrust\host_vector.h>
-
-extern "C" void hostSideSVD(const thrust::host_vector<double> &, thrust::host_vector<double> &, unsigned int, unsigned int, unsigned int);
-
-#endif
